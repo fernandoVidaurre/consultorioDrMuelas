2b3a4c928b35295394b563f8ce137df4a54c6064
julio vidaurre <juliofernandovidaurre@gmail.com>
2022-06-21 22:51:31 -0300

    subida del proyecto

---
+++ src/vista/principal.java
@@ -0,0 +1,13 @@
+package vista;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("*-*-*-* DR MUELAS *-*-*-*");
+		System.out.println("-------------------------");
+
+	}
+
+}
