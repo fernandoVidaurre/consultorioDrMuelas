e87e88a970e47ad6d3c12942437d76a20bc2231f
Franco <fran14korn@live.com>
2022-07-01 22:37:11 -0300

    Se agregaron las clases AdministradorDao y PacienteDao, para definir metodos que accedan a la base de datos

---
+++ src/Modelo/AdministradorDao.java
@@ -0,0 +1,5 @@
+package Modelo;
+
+public class AdministradorDao {
+	// aqui se escribiran los metodos que acceden a la bd por parte de Administrador
+}
---
+++ src/Modelo/PacienteDao.java
@@ -0,0 +1,5 @@
+package Modelo;
+
+public class PacienteDao {
+	// aqui se escribiran los metodos que acceden a la bd por parte de paciente
+}
