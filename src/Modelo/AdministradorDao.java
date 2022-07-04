8fc03d1edb70dcc6e2a743ed1492e020d97a573f
Franco <fran14korn@live.com>
2022-07-04 17:56:30 -0300

    Se agrego una clase de ejemplo, para los metodos de pacienteDao y administracionDao

--- src/Modelo/AdministradorDao.java
+++ src/Modelo/AdministradorDao.java
@@ -2,4 +2,8 @@ package Modelo;
 
 public class AdministradorDao {
 	// aqui se escribiran los metodos que acceden a la bd por parte de Administrador
+	
+	// ver fichas de pacientes
+	// cargar un paciente a emergencias
+	// generar informe
 }
--- src/Modelo/PacienteDao.java
+++ src/Modelo/PacienteDao.java
@@ -2,4 +2,8 @@ package Modelo;
 
 public class PacienteDao {
 	// aqui se escribiran los metodos que acceden a la bd por parte de paciente
+	
+	// crear turno
+	//dar de baja turno
+	//ver ficha de tratamientos
 }
